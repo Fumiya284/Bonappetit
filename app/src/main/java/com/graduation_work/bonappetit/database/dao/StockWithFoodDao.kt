ccef8cff72a28e88e380e6b4f5e0cec8bd00e4db
Fumiya284 <qxze1357@gmail.com>
2022-09-27 13:43:55 +0900

    StockAndFoodViewを作るQueryを変更した

--- app/src/main/java/com/graduation_work/bonappetit/database/dao/StockWithFoodDao.kt
+++ app/src/main/java/com/graduation_work/bonappetit/database/dao/StockWithFoodDao.kt
@@ -2,6 +2,7 @@ package com.graduation_work.bonappetit.database.dao
 
 import androidx.room.Dao
 import androidx.room.Query
+import androidx.room.RewriteQueriesToDropUnusedColumns
 import com.graduation_work.bonappetit.database.entities.StockWithFoodView
 
 @Dao
@@ -9,6 +10,6 @@ interface StockWithFoodDao {
 	@Query("SELECT * FROM stock_with_food")
 	suspend fun selectAll(): List<StockWithFoodView>
 	
-	@Query("SELECT * FROM stock_with_food WHERE name = :name")
+	@Query("SELECT * FROM stock_with_food WHERE food_name = :name")
 	suspend fun selectByName(name: String): List<StockWithFoodView>
 }
--- app/src/main/java/com/graduation_work/bonappetit/database/entities/StockWithFoodView.kt
+++ app/src/main/java/com/graduation_work/bonappetit/database/entities/StockWithFoodView.kt
@@ -6,19 +6,15 @@ import java.time.LocalDate
 
 @DatabaseView(
 	viewName = "stock_with_food",
-	value = "SELECT * FROM stock LEFT OUTER JOIN food ON stock.food_name = food.name"
+	value = "SELECT stock.id, stock.food_name, stock.count, food.unit, stock.`limit`, stock.best_or_expiry FROM stock LEFT OUTER JOIN food ON stock.food_name = food.name"
 )
 data class StockWithFoodView(
 	val id: Long,
-	@ColumnInfo(name = "name")
+	@ColumnInfo(name = "food_name")
 	val foodName: String,
 	val unit: String,
 	val count: Int,
 	val limit: LocalDate? = null,
 	@ColumnInfo(name = "best_or_expiry")
 	val bestOrExpiry: Boolean? = null
-){
-	fun toStockEntity(): StockEntity {
-		return StockEntity(id, foodName, count, limit, bestOrExpiry)
-	}
-}
+)
