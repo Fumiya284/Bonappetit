b1cdd8f0b5486cdff0cb39cce869bbd4d38e3105
Fumiya284 <qxze1357@gmail.com>
2022-10-27 12:19:31 +0900

    絞り込みのダイアログ実装した。ViewModelの生成方法がsingleなのが気になる。あとFragmentのwhenのelse何とかしたい。

--- app/src/main/java/com/graduation_work/bonappetit/ui/fragment/CategorySelectDFragment.kt
+++ app/src/main/java/com/graduation_work/bonappetit/ui/fragment/CategorySelectDFragment.kt
@@ -1,4 +1,24 @@
 package com.graduation_work.bonappetit.ui.fragment
 
-class CategorySelectDFragment {
+import android.app.AlertDialog
+import android.app.Dialog
+import android.os.Bundle
+import androidx.fragment.app.DialogFragment
+import com.graduation_work.bonappetit.ui.view_model.StockManagerViewModel
+import org.koin.java.KoinJavaComponent.inject
+
+class CategorySelectDFragment : DialogFragment() {
+	private val viewModel: StockManagerViewModel by inject(StockManagerViewModel::class.java)
+	
+	override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+		val categoryList = viewModel.categoryList.value.keys.toTypedArray()
+		val checkedItem = viewModel.categoryList.value.values.toBooleanArray()
+		
+		return AlertDialog.Builder(this.context).apply {
+			setTitle("食べ物の種類を選択してください")
+			setMultiChoiceItems(categoryList, checkedItem) { _, which, isChecked ->
+				viewModel.onDialogItemClick(categoryList[which], isChecked)
+			}
+		}.create()
+	}
 }
--- app/src/main/java/com/graduation_work/bonappetit/ui/fragment/StockManagerFragment.kt
+++ app/src/main/java/com/graduation_work/bonappetit/ui/fragment/StockManagerFragment.kt
@@ -5,18 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.graduation_work.bonappetit.databinding.StockManagerFragmentBinding
 import com.graduation_work.bonappetit.ui.adapter.StockListAdapter
 import com.graduation_work.bonappetit.ui.view_model.StockManagerViewModel
+import com.graduation_work.bonappetit.ui.view_model.StockManagerViewModel.Message
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
-import org.koin.androidx.viewmodel.ext.android.viewModel
+import org.koin.java.KoinJavaComponent.inject
 
 class StockManagerFragment: Fragment() {
-	private val viewModel: StockManagerViewModel by viewModel()
+	private val viewModel: StockManagerViewModel by inject(StockManagerViewModel::class.java)
 	private lateinit var stockListAdapter: StockListAdapter
 	
 	override fun onCreateView(
@@ -27,16 +29,30 @@ class StockManagerFragment: Fragment() {
 		return StockManagerFragmentBinding.inflate(inflater, container, false).also {
 			it.viewModel = viewModel
 			it.lifecycleOwner = viewLifecycleOwner
-			
 			it.stockListRv.apply {
 				layoutManager = GridLayoutManager(activity, 3, RecyclerView.VERTICAL, false)
 				stockListAdapter = StockListAdapter(viewLifecycleOwner, viewModel)
 				adapter = stockListAdapter
 			}
 		}.also {
-			viewModel.stockList.onEach {
-				stockListAdapter.submitList(it)
-			}.launchIn(lifecycleScope)
+			viewModel.message
+				.onEach { onMessage(it) }
+				.launchIn(lifecycleScope)
+			
+			viewModel.stockList
+				.onEach { stockListAdapter.submitList(it) }
+				.launchIn(lifecycleScope)
 		}.root
+	}
+	
+	private fun onMessage(message: Message) {
+		when (message) {
+			is Message.Search -> onMessageSearch(message)
+			else -> {}  // 参考にしたコードは拡張関数つくってelse句排除してたけどなんでできるのか分らないからとりあえず空のブロック置いとく 助けてください
+		}
+	}
+	
+	private fun onMessageSearch(message: Message.Search) {
+		CategorySelectDFragment().show(parentFragmentManager, "Search Dialog")
 	}
 }
