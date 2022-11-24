a345bcb7acec8e4510b2d4a592a99e5537d88070
Fumiya284 <qxze1357@gmail.com>
2022-11-24 18:49:19 +0900

    gitignoreに.idea/misc.xmlを追加した

--- app/src/main/java/com/graduation_work/bonappetit/ui/MainActivity.kt
+++ app/src/main/java/com/graduation_work/bonappetit/ui/MainActivity.kt
@@ -2,11 +2,7 @@ package com.graduation_work.bonappetit.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.android.material.tabs.TabLayoutMediator
 import com.graduation_work.bonappetit.R
-import com.graduation_work.bonappetit.ui.adapter.ViewPagerAdapter
-import com.graduation_work.bonappetit.databinding.ActivityMainBinding
-import com.graduation_work.bonappetit.test_utils.TestUtil
 
 class MainActivity : AppCompatActivity() {
 
