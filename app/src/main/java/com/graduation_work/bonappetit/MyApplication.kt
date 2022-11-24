b1af9df412598d215aa5f1c7468ab440b772e40b
Tetsuo0628 <k019c1263@g.neec.ac.jp>
2022-11-24 16:26:35 +0900

    changed background, buttoncolor, buttonlayout

--- app/src/main/java/com/graduation_work/bonappetit/MyApplication.kt
+++ app/src/main/java/com/graduation_work/bonappetit/MyApplication.kt
@@ -3,6 +3,19 @@ package com.graduation_work.bonappetit
 import android.app.Application
 import androidx.room.Room
 import com.graduation_work.bonappetit.data.database.AppDatabase
+<<<<<<< Updated upstream
+=======
+import com.graduation_work.bonappetit.data.repository.FoodRepositoryImpl
+import com.graduation_work.bonappetit.data.repository.StockRepositoryImpl
+import com.graduation_work.bonappetit.domain.repository.FoodRepository
+import com.graduation_work.bonappetit.domain.repository.StockRepository
+import com.graduation_work.bonappetit.domain.use_case.StockManagerUseCase
+import com.graduation_work.bonappetit.domain.use_case.StockRegisterUseCase
+import com.graduation_work.bonappetit.test_utils.TestUtil
+import com.graduation_work.bonappetit.ui.view_model.StockManagerViewModel
+import com.graduation_work.bonappetit.ui.view_model.StockRegisterViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+>>>>>>> Stashed changes
 import org.koin.core.context.startKoin
 
 class MyApplication : Application() {
